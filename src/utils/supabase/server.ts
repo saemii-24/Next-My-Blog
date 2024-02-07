64f5b75e92e56b768564fa57149ddae2e320b13f
saemii-24 <hims2419@gmail.com>
2024-02-07 11:36:56 +0900

    Feat: Supabase 초기 세팅

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,15 @@
+import { createServerClient } from '@supabase/ssr';
+
+export const createClient = (cookies: Partial<{ [key: string]: string }>) => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies[name];
+        },
+      },
+    },
+  );
+};
